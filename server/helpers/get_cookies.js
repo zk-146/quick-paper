b6c03d64c65a588825444641d6bf4dbd5f69fbdb
zk-146 <khanzaid1015@gmail.com>
2021-06-10 23:43:48 +0530

    Fixed bug for missing cookies

--- server/helpers/get_cookies.js
+++ server/helpers/get_cookies.js
@@ -1,11 +1,15 @@
 const get_cookies = (request) => {
   const cookies = {};
-  request.headers &&
-    request.headers.cookie.split(";").forEach(function (cookie) {
-      var parts = cookie.match(/(.*?)=(.*)$/);
-      cookies[parts[1].trim()] = (parts[2] || "").trim();
-    });
-  return cookies;
+  try {
+    request.headers &&
+      request.headers.cookie.split(";").forEach(function (cookie) {
+        var parts = cookie.match(/(.*?)=(.*)$/);
+        cookies[parts[1].trim()] = (parts[2] || "").trim();
+      });
+    return cookies;
+  } catch (err) {
+    throw new Error("Error occurred while getting cookies");
+  }
 };
 
 module.exports = get_cookies;
