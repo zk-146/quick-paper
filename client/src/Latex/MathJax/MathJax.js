c7618fb369b74b52e72386b35b64f1ed0bd65aca
zk-146 <khanzaid1015@gmail.com>
2021-06-15 18:24:52 +0530

    Remove unused files

--- client/src/Latex/MathJax/MathJax.js
+++
@@ -1,7 +0,0 @@
-const MathJax = () => {
-  const script = document.createElement("script");
-  script.type = "text/javascript";
-  script.src = "https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js";
-  script.async = true;
-};
-export default MathJax;
